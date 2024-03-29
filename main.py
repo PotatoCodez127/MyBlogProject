b7ad263b4e683c13c1b8c978f8a5fd0226d7b0b7
Jeandre <jeandre.c127@gmail.com>
2024-03-29 14:14:19 +0200

    Adding a Database instead of an API and .env variables

--- main.py
+++ main.py
@@ -1,26 +1,134 @@
-from flask import Flask, render_template
-import requests
+import datetime
 
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+from dotenv import load_dotenv, find_dotenv
+import os
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
 app = Flask(__name__)
-
-# Get json data from API - Data=Post
-url_response = 'https://api.npoint.io/45e1b47a1d9ca0fbc906'
-response = requests.get(url_response)
-json_data = response.json()
-
-date = ['1 August 2021', '24 October 2021', '6 December 2021']
-
-for j in range(3):
-    temp = json_data[j]
-    temp['date'] = date[j]
-    temp['name'] = 'Jeandre Coetzee'
+app.config['SECRET_KEY'] = os.environ.get(Flask_SECRET_KEY)
+Bootstrap5(app)
+ckeditor = CKEditor(app)
 
 
-@app.route("/")
-def get_all_posts(data=json_data):
-    return render_template("index.html", data=data)
+# CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
 
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CONFIGURE TABLE
+class BlogPost(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    subtitle: Mapped[str] = mapped_column(String(250), nullable=False)
+    date: Mapped[str] = mapped_column(String(250), nullable=False)
+    body: Mapped[str] = mapped_column(Text, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+# create form
+class NewPostForm(FlaskForm):
+    title = StringField(u'Title', validators=[validators.input_required()])
+    subtitle = StringField(u'Subtitle', validators=[validators.input_required()])
+    author = StringField(u'Name of author', validators=[validators.input_required()])
+    img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
+    body = CKEditorField("Blog Content", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+    date: str = None
+
+
+@app.route('/')
+def get_all_posts():
+    query = db.select(BlogPost)
+    response = db.session.execute(query)
+    posts = response.scalars().all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route('/<post_id>')
+def show_post(post_id):
+    requested_post = db.get_or_404(BlogPost, post_id)
+    return render_template("post.html", post=requested_post)
+
+
+# TODO: add_new_post() to create a new blog post
+@app.route("/new_post", methods=["GET", "POST"])
+def add_new_post():
+    form = NewPostForm()
+    heading = "New Post"
+    if request.method == "POST":
+        now = datetime.datetime.now()
+        month_name = now.strftime("%B")
+        date_number = now.strftime("%d")
+        full_year = now.strftime("%Y")
+        blog_post = BlogPost(
+            title=form.title.data,
+            subtitle=form.subtitle.data,
+            author=form.author.data,
+            img_url=form.img_url.data,
+            body=form.body.data,
+            date=f"{month_name} {date_number}, {full_year}",
+        )
+        db.session.add(blog_post)
+        db.session.commit()
+        return redirect("/")
+    return render_template("./make-post.html", form=form, heading=heading)
+
+
+# TODO: edit_post() to change an existing blog post
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    query = db.select(BlogPost).where(BlogPost.id == post_id)
+    post = db.session.execute(query).scalar()
+    edit_form = NewPostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body
+    )
+    heading = "Edit Post"
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("./make-post.html", form=edit_form, heading=heading, post_id=post_id)
+
+
+# TODO: delete_post() to remove a blog post from the database
+@app.route("/delete/<int:post_id>")
+def delete_post(post_id):
+    query = db.delete(BlogPost).where(BlogPost.id == post_id)
+    db.session.execute(query)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
+
+# Below is the code from previous lessons. No changes needed.
 @app.route("/about")
 def about():
     return render_template("about.html")
@@ -29,15 +137,6 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
-
-
-@app.route("/post/<int:index_var>")
-def show_post(index_var):
-    requested_page = None
-    for blog in json_data:
-        if blog['id'] == index_var:
-            requested_page = blog
-    return render_template("post.html", data=requested_page)
 
 
 if __name__ == "__main__":
